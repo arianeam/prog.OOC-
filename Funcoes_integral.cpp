591b6c408a46672a9c26f1723789535952122599
arianeam <47660205+arianeam@users.noreply.github.com>
2020-08-17 20:35:58 -0300

    Add files via upload

---
+++ Funcoes_integral.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Funcoes_integral.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
