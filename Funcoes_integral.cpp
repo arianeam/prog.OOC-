9f2e5d22a72a52980d3ed54300678c9a3a451da7
arianeam <47660205+arianeam@users.noreply.github.com>
2020-08-25 21:00:31 -0300

    Add files via upload

--- Funcoes_integral.cpp
+++ Funcoes_integral.cpp
@@ -10,46 +10,51 @@
 #include <vector>
 using namespace std;
 
-
-
-class Funcao{
+class Funcao {
 public:
 
 	virtual double operator()(void)=0;
 	virtual double getvalue(void)=0;
-	virtual ~Funcao(){}
 
+	static double integrar(Funcao* f, double x0, double x1, double step)
+	{
+
+		return 0;
+	}
+
+	virtual ~Funcao() {
+	}
+
+private:
+	double i;
 
 };
 
 //----------------------------------------------------
 typedef vector<Funcao*> FuncaoVector;
 
-class FuncaoAgregada: public Funcao{
+class FuncaoAgregada: public Funcao {
 public:
 
-
-
-	void agrega (Funcao* f)
-	{
+	void agrega(Funcao *f) {
 		fv.push_back(f);
 
 	}
 
-	double operator()(void)
-	{
-        fx = 0;
-		for(FuncaoVector::iterator it = fv.begin(); it != fv.end(); it++){
-				 cout<<"f(x) agregada: "<< static_cast<Funcao*>(*it) ->getvalue()<<endl;
-				 fx += static_cast<Funcao*>(*it) ->getvalue();
-			}
-		cout<<"f(x)= "<< fx<<endl;
+	double operator()(void) {
+		fx = 0;
+		for (FuncaoVector::iterator it = fv.begin(); it != fv.end(); it++) {
+			cout << "f(x) agregada: " << static_cast<Funcao*>(*it)->getvalue()
+					<< endl;
+			fx += static_cast<Funcao*>(*it)->getvalue();
+		}
+
+		cout << "f(x)= " << fx << endl;
 
 		return fx;
 	}
 
-	double getvalue(void)
-	{
+	double getvalue(void) {
 		return fx;
 	}
 
@@ -58,47 +63,61 @@ private:
 	double fx;
 };
 
-
-
 //---------------------------------------------------
 
-class Constante: public Funcao{
+class Constante: public Funcao {
 public:
 
-	Constante(double v): value(v){}
+	Constante(double v) :
+			value(v) {
+	}
 
-	double operator()(void)
-	{
-		cout<< "Constante"<< endl;
-		cout<< "f(x) = "<< value << endl;
-		cout<< endl;
+	double operator()(void) {
+		cout << "Constante" << endl;
+		cout << "f(x) = " << value << endl;
+		cout << endl;
 		return value;
 	}
-double getvalue(void)
-{
-	return value;
-}
+	double getvalue(void) {
+		return value;
+	}
 
 private:
 	double value;
 };
 
 //------------------------------------------------------
+class Escalar: public Funcao {
+public:
 
+	Escalar(){}
+	Escalar(double v): value(v){}
+
+
+
+	double operator()(void) {
+
+		return 0;
+	}
+	double getvalue(void) {
+		return value;
+	}
+
+private:
+	double value;
+};
+//------------------------------------------------------
 
 int main() {
 
-
-   Constante i(5.13);
-   i();
-   Constante j(5.13);
-      j();
-   FuncaoAgregada fa;
-   fa.agrega(&i);
-   fa.agrega(&j);
-   fa();
-
-
+	Constante i(5.13);
+	i();
+	Constante j(5.13);
+	j();
+	FuncaoAgregada fa;
+	fa.agrega(&i);
+	fa.agrega(&j);
+	fa();
 
 	return 0;
 }
