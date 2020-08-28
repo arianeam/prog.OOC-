5d2933d934619a186b440f74f8d5b5c5b99f060d
arianeam <47660205+arianeam@users.noreply.github.com>
2020-08-28 20:16:21 -0300

    Add files via upload

--- Funcoes_integral.cpp
+++ Funcoes_integral.cpp
@@ -9,8 +9,11 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <iomanip>
 
 using namespace std;
+
+void teste();
 
 class Funcao {
 public:
@@ -22,19 +25,27 @@ public:
 	virtual double operator()(double x)=0;
 
 	static double integrar(Funcao *f, double x0, double x1, double step) {
-		//Integral = (step/2).(f(x0) + 2.soma(f(xi)) + f(xn)) , sendo i=1 até i=n-1
 
-		double soma, fx0, fxn;
+		/*Integral = (step/2).(f(x0) + 2.soma(f(xi)) + f(xn)) , sendo i=1 até i=n-1*/
+
+		double soma, fx0, fxn, x = x0;
 		double k = x0 + step;
+
 		cout << endl;
 		cout << "Passo: " << step << endl;
 		cout << "Intervalo de integração : [" << x0 << "," << x1 << "]" << endl;
 		cout << endl;
 
-		while (k < x1) {
+		while (x <= x1)
+		{
+			cout << "f(" << x << ")= " << f->operator()(x) << endl;
+			x += step;
+		}
+
+		while (k < x1)
+		{
 
 			soma += f->operator()(k);
-			cout << "soma = " << soma << endl;
 			k = k + step;
 		}
 
@@ -42,7 +53,6 @@ public:
 		fxn = f->operator()(x1);
 
 		i = (step / 2.0) * (fx0 + 2 * soma + fxn);
-		cout << "integral = " << i << endl;
 
 		return i;
 	}
@@ -65,22 +75,24 @@ public:
 
 	FuncaoAgregada() {}
 
-	void agrega(Funcao *f) { fv.push_back(f); }
+	void agrega(Funcao *f)
+	{
+		fv.push_back(f);
+	}
 
-	double operator()(double x) {
+	double operator()(double x)
+	{
 
 		double fx = 0;
 
-		for (FuncaoVector::iterator it = fv.begin(); it != fv.end(); it++) {
-
-			static_cast<Funcao*>(*it)->operator()(x);
-			cout << "f(x) agregada: "<< static_cast<Funcao*>(*it)->operator()(x) << endl;
+		for (FuncaoVector::iterator it = fv.begin(); it != fv.end(); it++)
+		{
 			fx += static_cast<Funcao*>(*it)->operator()(x);
 		}
 
-		cout << "f(" << x << ")= " << fx << endl;
 		return fx;
 	}
+
 
 private:
 	FuncaoVector fv;
@@ -91,16 +103,11 @@ private:
 class Constante: public Funcao {
 public:
 
-	Constante(): value(0) {
-	}
-	Constante(double v) :
-			value(v) {
-	}
+	Constante() : value(0) {}
+	Constante(double v) : value(v) {}
 
-	double operator()(double x) {
-		cout << "Constante" << endl;
-		cout << "C = " << value << endl;
-		cout << endl;
+	double operator()(double x)
+	{
 		return value;
 	}
 
@@ -112,33 +119,23 @@ private:
 class Escalar: public Funcao {
 public:
 
-	Escalar():a(0), f(0) {
-	}
-	Escalar(double v) :
-			a(v), f(0) {
-	}
-	Escalar(Funcao *func) :
-			a(0), f(func) {
-	}
-	Escalar(Funcao *func, double v) :
-			a(v), f(func) {
-	}
+	Escalar() : a(1), f(0) {}
+	Escalar(double v) : a(v), f(0) {}
+	Escalar(Funcao *func) : a(1), f(func) {}
+	Escalar(Funcao *func, double v) : a(v), f(func) {}
 
-	double operator()(double x) {
+	double operator()(double x)
+	{
 
 		double fx = 0;
 
 		if (f != NULL)
 		{
 			x = f->operator()(x);
-			cout<< "x = "<< x<< endl;
 		}
-		cout << "Escalar" << endl;
-		cout << "f(x) =" << a << ".x" << endl;
-		cout << endl;
+
 		fx = a * x;
-		cout << "x = " << x << endl;
-		cout << a << ".x=" << fx << endl;
+
 		return fx;
 	}
 
@@ -149,28 +146,23 @@ private:
 //------------------------------------------------------
 class Seno: public Funcao {
 public:
-	Seno(): value(1), f(0) {
-	}
-	Seno(double v) :
-			value(v), f(0) {
-	}
-	Seno(Funcao *func) :
-			value(1), f(func) {
-	}
-	Seno(double v, Funcao *func) :
-			value(v), f(func) {
-	}
+	Seno() : value(1), f(0) {}
+	Seno(double v) : value(v), f(0) {}
+	Seno(Funcao *func) : value(1), f(func) {}
+	Seno(double v, Funcao *func) : value(v), f(func) {}
 
-	double operator()(double x) {
+	double operator()(double x)
+	{
 
 		double fx = 0;
-		if(f!= NULL)
+
+		if (f != NULL)
 		{
 			x = f->operator()(x);
 		}
-		cout << "f(" << x << ")= "<< value<<".sen(" << x << ")" << endl;
-		fx = sin(x);
-		cout << "f(" << x << ")= " << fx << endl;
+
+		fx = value * sin(x);
+
 		return fx;
 	}
 
@@ -184,29 +176,23 @@ private:
 
 class Cosseno: public Funcao {
 public:
-	Cosseno(): value(1), f(0) {
-	}
-	Cosseno(double v) :
-			value(v), f(0) {
-	}
-	Cosseno(Funcao *func) :
-			value(1), f(func) {
-	}
-	Cosseno(double v, Funcao *func) :
-			value(v), f(func) {
-	}
+	Cosseno() : value(1), f(0) {}
+	Cosseno(double v) : value(v), f(0) {}
+	Cosseno(Funcao *func) :	value(1), f(func) {}
+	Cosseno(double v, Funcao *func) : value(v), f(func) {}
 
-	double operator()(double x) {
+	double operator()(double x)
+	{
 
 		double fx = 0;
-		if(f!= NULL)
+
+		if (f != NULL)
 		{
-					x = f->operator()(x);
+			x = f->operator()(x);
 		}
 
-		cout << "f(" << x << ")= "<<value<<".cos(" << x << ")" << endl;
-		fx = value*cos(x);
-		cout << "f(" << x << ")= " << fx << endl;
+		fx = value * cos(x);
+
 		return fx;
 	}
 
@@ -220,30 +206,22 @@ private:
 class Potencial: public Funcao {
 public:
 
-	Potencial(): a(0), f(0){
-	}
-	Potencial(double v) :
-			a(v),f(0) {
+	Potencial() : a(1), f(0) {}
+	Potencial(double v) : a(v), f(0) {}
+	Potencial(Funcao *func) : a(1), f(func) {}
+	Potencial(double v, Funcao *func) :	a(v), f(func) {}
 
-	}
-	Potencial(Funcao *func) :
-			a(0), f(func) {
-	}
-	Potencial(double v, Funcao *func) :
-			a(v), f(func) {
-	}
-
-	double operator()(double x) {
+	double operator()(double x)
+	{
 
 		double fx = 0;
 
-		if (f != NULL) {
+		if (f != NULL)
+		{
 			x = f->operator()(x);
 		}
-		cout << "f(x) = x^" << a << endl;
-		cout << "f(" << x << ")= " << x << "^" << a << endl;
+
 		fx = pow(x, a);
-		cout << "potenciacao resultado: " << fx << endl;
 
 		return fx;
 	}
@@ -260,31 +238,22 @@ private:
 class Exponencial: public Funcao {
 public:
 
-	Exponencial() {
-	}
-	Exponencial(double v) :
-			a(v), f(0) {
-	}
-	Exponencial(Funcao *func) :
-			f(func) {
-	}
-	Exponencial(double v, Funcao *func) :
-			a(v), f(func) {
-	}
+	Exponencial() {}
+	Exponencial(double v) :	a(v), f(0) {}
+	Exponencial(Funcao *func) :	a(1), f(func) {}
+	Exponencial(double v, Funcao *func) : a(v), f(func) {}
 
-	double operator()(double x) {
+	double operator()(double x)
+	{
 
 		double fx = 0;
-		if(f!= NULL)
+
+		if (f != NULL)
 		{
 			x = f->operator()(x);
 		}
 
-
-		cout << "f(x) = " << a << "^x" << endl;
-		cout << "f(" << x << ")= " << a << "^" << x << endl;
 		fx = pow(a, x);
-		cout << "exponencial resultado: " << fx << endl;
 
 		return fx;
 	}
@@ -300,33 +269,35 @@ private:
 
 int main() {
 
-	double x0, x1, step;
-	x0 = 0;
-		x1 = 5;
-		step = 0.0001;
-
-	Escalar a(new Potencial(2), 1);
-	Constante b(5);
-	FuncaoAgregada f;
-	f.agrega(&a);
-	f.agrega(&b);
-
-	Escalar c(2);
-	Constante d(-1);
-	FuncaoAgregada e;
-	e.agrega(&c);
-	e.agrega(&d);
-
-	Seno s(&e);
-	Escalar h(&s,5);
-	FuncaoAgregada i;
-	i.agrega(&h);
-
-	FuncaoAgregada m;
-	m.agrega(&f);
-	m.agrega(&i);
-
-	Funcao::integrar(&m, 0, 5, 0.01);
+	teste();
 
 	return 0;
+}
+
+void teste() {
+
+	double step = 0.01, x0 = 0.0, x1 = 5.0;
+	/* Com um step maior a precisão da integral aumenta
+	 Para a seguinte funcao um step de 0.001 já alcança o valor da integral calculada
+	 de forma manual */
+
+	Potencial a(2); //x^2
+
+	Escalar b(2);   //2x
+	Constante c(-1);
+	FuncaoAgregada d;
+	d.agrega(&b);
+	d.agrega(&c);
+	Seno e(&d); //Seno(2x - 1)
+	Escalar f(&e, 5); //5.Seno(2x - 1)
+
+	Constante g(5);
+
+	FuncaoAgregada h; //x^2 + 5.Seno(2x - 1) + 5
+	h.agrega(&a);
+	h.agrega(&f);
+	h.agrega(&g);
+
+	cout << "Intregal= " << Funcao::integrar(&h, x0, x1, step) << endl;
+
 }
