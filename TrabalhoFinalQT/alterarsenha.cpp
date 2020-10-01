062a5c5f42240e04ccf8a190d5d8829f3854057c
arianeam <47660205+arianeam@users.noreply.github.com>
2020-10-01 14:44:43 -0300

    Add files via upload

--- TrabalhoFinalQT/alterarsenha.cpp
+++ TrabalhoFinalQT/alterarsenha.cpp
@@ -9,18 +9,22 @@ alterarSenha::alterarSenha(QWidget *parent) :
 
     ui->novaSenhalineEdit->setDisabled(true);
     ui->confirmarlineEdit->setDisabled(true);
+    ui->saveBtn->setDisabled(true);
+
     connect(ui->senhaAtuallineEdit, SIGNAL(textChanged(const QString &)),
             this, SLOT(on_senhaAtuallineEdit_textChanged(const QString &)));
 
+    connect(ui->novaSenhalineEdit, SIGNAL(textChanged(const QString &)),
+            this, SLOT(on_novaSenhalineEdit_textChanged(const QString &)));
+
+    connect(ui->confirmarlineEdit, SIGNAL(textChanged(const QString &)),
+            this, SLOT(on_confirmarlineEdit_textChanged(const QString &)));
 }
 
 alterarSenha::~alterarSenha()
 {
     delete ui;
 }
-
-
-
 
 
 void alterarSenha::on_senhaAtuallineEdit_textChanged(const QString &arg1)
@@ -43,7 +47,75 @@ void alterarSenha::on_senhaAtuallineEdit_textChanged(const QString &arg1)
             if(cont>0){
                 ui->novaSenhalineEdit->setEnabled(true);
                 ui->avisolabel->setText("Insira nova senha");
+            }else{
+
+                ui->novaSenhalineEdit->setDisabled(true);
+                ui->avisolabel->setText("Senha incorreta!");
             }
         }
+    }
+}
+
+void alterarSenha::on_novaSenhalineEdit_textChanged(const QString &arg1)
+{
+    if(!arg1.isEmpty()){
+
+        QString nova_senha = ui->novaSenhalineEdit->text();
+
+        if(nova_senha.size()>=5){
+            ui->confirmarlineEdit->setEnabled(true);
+            ui->avisolabel_2->setText("Confirme a nova senha");
+        }else{
+
+            ui->confirmarlineEdit->setDisabled(true);
+            ui->avisolabel_2->setText("A senha deve ter pelo menos 5 caracteres!");
+        }
+    }
+}
+
+
+void alterarSenha::on_confirmarlineEdit_textChanged(const QString &arg1)
+{
+    if(!arg1.isEmpty()){
+
+        QString nova_senha = ui->novaSenhalineEdit->text();
+        QString confirmar_senha = ui->confirmarlineEdit->text();
+
+        if(confirmar_senha==nova_senha){
+
+            ui->avisolabel_2->setText("Salve as alterações!");
+            ui->saveBtn->setEnabled(true);
+
+        }else{
+
+            ui->avisolabel_2->setText("Confirme a nova senha!");
+            ui->saveBtn->setDisabled(true);
+
+        }
+    }
+
+}
+
+void alterarSenha::on_cancelBtn_clicked()
+{
+    this->close();
+}
+
+void alterarSenha::on_saveBtn_clicked()
+{
+    QString nova_senha = ui->confirmarlineEdit->text();
+    QString senha_antiga = ui->senhaAtuallineEdit->text();
+
+    QSqlQuery query;
+
+    query.prepare("update tb_login set senha='"+nova_senha+"' where senha='"+senha_antiga+"'");
+
+    if(query.exec()){
+
+        QMessageBox::information(this,"","Senha alterada com suceso!");
+    }else{
+
+        QMessageBox::warning(this,"ERRO","Erro ao alterar senha!");
+
     }
 }
--- TrabalhoFinalQT/alterarsenha.h
+++ TrabalhoFinalQT/alterarsenha.h
@@ -20,6 +20,15 @@ private slots:
 
     void on_senhaAtuallineEdit_textChanged(const QString &arg1);
 
+    void on_novaSenhalineEdit_textChanged(const QString &arg1);
+
+
+    void on_confirmarlineEdit_textChanged(const QString &arg1);
+
+    void on_cancelBtn_clicked();
+
+    void on_saveBtn_clicked();
+
 private:
     Ui::alterarSenha *ui;
 };
